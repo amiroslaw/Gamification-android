16e19ce146bc4084ea7d441d1ced2957f8788023
miro <arek47@gmail.com>
2017-06-13 20:43:07 +0200

    adding new style

--- app/src/main/java/xyz/miroslaw/gamification_android/menu/MenuActivity.java
+++ app/src/main/java/xyz/miroslaw/gamification_android/menu/MenuActivity.java
@@ -21,9 +21,18 @@ public class MenuActivity extends AppCompatActivity {
 
         createListView();
     }
+//    error: failed to push some refs to 'https://github.com/amiroslaw/Gamification-android.git'
+//    hint: Updates were rejected because the remote contains work that you do
+//    To https://github.com/amiroslaw/Gamification-android.git
+//    hint: not have locally. This is usually caused by another repository pushing
+//!	refs/heads/master:refs/heads/master	[rejected] (fetch first)
+//    hint: to the same ref. You may want to first integrate the remote changes
+//            Done
+//    hint: (e.g., 'git pull ...') before pushing again.
+//            hint: See the 'Note about fast-forwards' in 'git push --help' for details.
 
     private void createListView() {
         String[] menuNames = getResources().getStringArray(R.array.menu_items);
-        menuItems.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuNames));
+        menuItems.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menuNames));
     }
 }
